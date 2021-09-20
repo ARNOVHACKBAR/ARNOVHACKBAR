bbdf6312f9f31321bf57805ba9d8c6d2aad172b2
ARNOV HACK BAR <88029368+ARNOVHACKBAR@users.noreply.github.com>
2021-09-20 09:14:29 +0600

    Add files via upload

---
+++ 0188.py
@@ -0,0 +1,28 @@
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+color_off="\033[0m"       # Text Reset
+space=" "
+logo=red+("""   ____ _______ _____  
+  / __ \__   __|  __ \ 
+ | |  | | | |  | |__) |                    
+ | |  | | | |  |  ___/                    #A.VIRUS#
+ | |__| | | |  | |                       
+  \____/  |_|  |_|     
+                       
+             A.VIRUS        """)
+print(logo)
+print(yellow+"≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠Developed By :arnov chowdhury≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠THIS IS AVL TOOL≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠")             
+
+
+x=int(input("enter your Terget number :"))
+
+y=int(input("enter your Time :"))
+
+
+print("Terget number all sms\n\n1.রবি স্পেশাল অফার আজ পুরো ১০জিবি ৭দিনের জন্য ৳১২৯ ঘ্যাচাংরিচার্জে")
